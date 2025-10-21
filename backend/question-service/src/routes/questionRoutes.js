c333112fdd64ff900615899c403d05210b19d209
Chi Thanh <e1129981@u.nus.edu>
2025-10-22 01:31:07 +0800

    add functionality to fetch question based on id

--- backend/question-service/src/routes/questionRoutes.js
+++ backend/question-service/src/routes/questionRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { fetchAllQuestions, pickQuestion, seedQuestions } = require('../controllers/questionController')
+const { fetchAllQuestions, pickQuestion, seedQuestions, getQuestionById } = require('../controllers/questionController')
 
 router.route('/').get(fetchAllQuestions)
 router.route('/pick').get(pickQuestion)
 router.route('/seed').post(seedQuestions)
+router.route('/:id').get(getQuestionById)
 
 module.exports = router
