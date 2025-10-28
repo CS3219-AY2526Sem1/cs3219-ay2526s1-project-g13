0e3b49b026a9c0c1d5d79d4629a9c76bf8d8560f
k1b1t0 <nguyenminhduc2882005@gmail.com>
2025-10-28 12:00:43 +0800

    Add Agora Token generator

---
+++ backend/video-call-service/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const {RtcTokenBuilder, RtcRole} = require('agora-access-token');
+const dotenv = require('dotenv');
+
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT
+const APP_ID = process.env.APP_ID
+const APP_CERTIFICATE = process.env.APP_CERTIFICATE
+
+const nocache = (_, resp, next) => {
+  resp.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  resp.header('Expires', '-1');
+  resp.header('Pragma', 'no-cache');
+  next();
+}
+
+const generateRTCToken = (req, resp) => {
+  resp.header('Access-Control-Allow-Origin', '*');
+  const roomId = req.params.roomid;
+  // channel - room id 
+  if (!roomId) {
+    return resp.status(500).json({ 'error': 'roomId is required' });
+  }
+  // user id
+  let uid = req.params.uid;
+  if(!uid || uid === '') {
+    return resp.status(500).json({ 'error': 'uid is required' });
+  }
+  // get role
+  let role = RtcRole.PUBLISHER
+  console.log(">>> Role is always PUBLISHER")
+  // check expire time for token
+  let expireTime = req.query.expiry;
+  if (!expireTime || expireTime === '') {
+    expireTime = 3600;
+  } else {
+    expireTime = parseInt(expireTime, 10);
+  }
+  // calculate expire time
+  const currentTime = Math.floor(Date.now() / 1000);
+  const privilegeExpireTime = currentTime + expireTime;
+  // build token
+  let token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, roomId, uid, role, privilegeExpireTime)
+  console.log(">>> Create token successfully!")
+  // return token
+  return resp.json({ 'rtcToken': token });
+}
+
+app.get('/v1/rtc/:roomid/:uid', nocache , generateRTCToken)
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
