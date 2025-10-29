6f464be0f4ab78fd2e42f9f127dd91f55b8c6654
songgthu <e1082977@u.nus.edu>
2025-10-29 09:51:54 +0800

    Add protected route for QuestionViewPage

--- frontend/src/app/question/[id]/page.tsx
+++ frontend/src/app/question/[id]/page.tsx
@@ -1,11 +1,24 @@
+"use client";
+
 import QuestionCard from "@/components/question/question-card";
 import Navbar from "@/components/ui/nav-bar";
+import { useAuthContext } from "@/contexts/auth-context";
+import ProtectedRoute from "@/components/auth/protected-route";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-export default async function QuestionViewPage() {
+export default function QuestionViewPage() {
+  const router = useRouter();
+  const { user, isAdmin } = useAuthContext();
+  useEffect(() => {
+    if (!user || !isAdmin) {
+      router.push("/");
+    }
+  }, [user, isAdmin, router]);
   return (
-    <div>
+    <ProtectedRoute>
       <Navbar />
       <QuestionCard />
-    </div>
+    </ProtectedRoute>
   );
 }
