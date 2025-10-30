ea0d4ec3bd5bbcd0c252cf39316a0b4406562503
songgthu <e1082977@u.nus.edu>
2025-10-30 16:20:18 +0800

    Add function to auto-create admin user

--- backend/user-service/src/server.js
+++ backend/user-service/src/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("./model/user-model")
 
 const authRoutes = require("./routes/auth-routes");
 
@@ -25,6 +27,30 @@ app.use("/v1", authRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
+  .then(async () => {
+    console.log("MongoDB connected");
+
+    const adminUsername = process.env.ADMIN_USERNAME;
+    const adminEmail = process.env.ADMIN_EMAIL;
+    const adminPassword = process.env.ADMIN_PW;
+
+    const existingAdmin = await User.findOne({ username: adminUsername });
+
+    if (!existingAdmin) {
+      const hashedPassword = await bcrypt.hash(adminPassword, 10);
+      const admin = new User({
+        username: adminUsername,
+        email: adminEmail,
+        password: hashedPassword,
+        role: "admin",
+        verified: true
+      });
+      await admin.save();
+      console.log("Default admin user created");
+    } else {
+      console.log("Admin user already exists");
+    }
+  })
   .catch((err) => console.log("MongoDB connection error:", err));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
